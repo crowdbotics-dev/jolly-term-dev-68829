d7226e31460dbfe3c9798a4bd1fd3c7588dd6a0b
Crowdbotics <engineering@crowdbotics.com>
2023-03-12 00:12:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/jolly_term_dev_68829/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jolly_term_dev_68829;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "jolly_term_dev_68829";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jolly-term-dev-68829",
+  url: "https://jolly-term-dev-68829.botics.co",
+  api: "https://jolly-term-dev-68829.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
